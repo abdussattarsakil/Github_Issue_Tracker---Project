fdaf3dc62f3f05ba64964fe0af97886b4aeead01
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-03-09 06:13:43 +0600

    card design

--- script/main-page.js
+++ script/main-page.js
@@ -1,1 +1,34 @@
 console.log("connected");
+
+const loadData = () => {
+    const url=`https://phi-lab-server.vercel.app/api/v1/lab/issues`
+    fetch(url)
+    .then(res=>res.json())
+    .then(json=>displayLoadData(json.data))
+}
+
+
+const displayLoadData=(data)=>{
+    //console.log(data)
+    const cardContainer=document.getElementById("card-container")
+    cardContainer.innerHTML=``;
+
+    data.forEach(info => {
+        console.log(info);
+        
+        const div=document.createElement("div")
+        div.innerHTML=`
+        
+        `
+        cardContainer.append(div)
+    });
+}
+
+
+
+
+
+
+
+
+loadData();
