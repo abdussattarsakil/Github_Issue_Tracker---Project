39a413d73aa82c0c288a4d3b25f9925d82ad6f87
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-03-09 16:30:11 +0600

    dynamic card design half done

--- script/main-page.js
+++ script/main-page.js
@@ -23,14 +23,20 @@ const displayLoadData = (data) => {
             <div class=" bg-slate-50 shadow-lg p-6 h-full">
                 <div class="flex justify-between">
                     <img src="assets/Open-Status.png">
-                    <p class="text-red-500 bg-red-300 px-5 rounded-xl">HIGH</p>
+                    <p class="text-red-500 bg-red-100 px-5 rounded-xl">${info.priority}</p>
                 </div>
                 <h1 class="mt-2 font-semibold text-sm">${info.title}</h1>
                 <h3 class="my-2 text-[12px] text-[#64748B]">
                 ${info.description}</h3>
-                <div class="flex gap-3">
-                    <p>${info.labels[0]}</p>
-                    <P>${info.labels[1]}</P>
+                <div class="flex gap-1 lg:gap-3">
+                    ${info.labels[0]=="bug"?`<p class="text-red-500 text-[12px] border border-red-200 px-3 font-medium rounded-full py-1 bg-red-50" ><i class="fa-solid fa-bug"></i> BUG</p>`:""}
+
+                    ${info.labels[0]=="enhancement"?`<p class="text-green-500 text-[12px] border border-green-200 px-3 font-medium rounded-full py-1 bg-green-50" ><i class="fa-solid fa-wand-magic-sparkles"></i> ENHANCEMENT</p>`:""}
+
+                    ${info.labels[0]=="documentation"?`<p class="text-blue-500 text-[12px] border border-blue-200 px-3 font-medium rounded-full py-1 bg-blue-50" ><i class="fa-brands fa-readme"></i>  DOCUMENTATION</p>`:""}
+                    
+
+                    ${info.labels[1]?`<P class="text-yellow-600 text-[12px] border border-yellow-200 px-3 font-medium rounded-full py-1 bg-yellow-50"><i class="fa-solid fa-life-ring"></i> HELP WANTED</P>`:""}
                 </div>
                 <div class="card-divider"></div>
                 <p class="text-[12px] text-[#64748B] my-2">#1 by<span>
